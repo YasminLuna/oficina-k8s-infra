afad4ae3da04313b4edde00b88155f829c549052
YasminLuna <65403619+YasminLuna@users.noreply.github.com>
2026-09-14 18:03:54 -0300

    infra: prepare EKS subnets for public load balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,11 +17,18 @@ module "vpc" {
   private_subnets = ["10.42.1.0/24", "10.42.2.0/24"]
   public_subnets  = ["10.42.101.0/24", "10.42.102.0/24"]
 
-  # Ambiente academico: evita NAT Gateway para reduzir custo e permissoes.
-  enable_nat_gateway       = false
-  map_public_ip_on_launch  = true
-  enable_dns_hostnames     = true
-  enable_dns_support       = true
+  enable_nat_gateway      = false
+  map_public_ip_on_launch = true
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
 }
 
 module "eks" {
@@ -34,11 +41,9 @@ module "eks" {
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
-  # Ambiente academico: nodes em subnets publicas, sem NAT Gateway.
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
 
-  # Evita dependencias KMS/CloudWatch que nao sao obrigatorias para o desafio.
   create_cloudwatch_log_group = false
   cluster_enabled_log_types   = []
   create_kms_key              = false
