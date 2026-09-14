1006518bf594bcc4c085eb4869d55ef5d2e6ba0e
YasminLuna <65403619+YasminLuna@users.noreply.github.com>
2026-09-14 16:52:22 -0300

    chore: update EKS version and reduce lab footprint

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,28 +8,34 @@ module "vpc" {
   name    = "oficina-${var.environment}"
   cidr    = "10.42.0.0/16"
   azs     = slice(data.aws_availability_zones.available.names, 0, 2)
+
   private_subnets = ["10.42.1.0/24", "10.42.2.0/24"]
   public_subnets  = ["10.42.101.0/24", "10.42.102.0/24"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  enable_dns_hostnames = true
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_hostnames   = true
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
+
   cluster_name    = "${var.cluster_name}-${var.environment}"
-  cluster_version = "1.32"
-  cluster_endpoint_public_access = true
+  cluster_version = "1.35"
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_groups = {
     default = {
       instance_types = ["t3.medium"]
-      min_size = 2
-      max_size = 4
-      desired_size = 2
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
     }
   }
 }
@@ -37,5 +43,8 @@ module "eks" {
 resource "aws_ecr_repository" "api" {
   name                 = "oficina-api-${var.environment}"
   image_tag_mutability = "MUTABLE"
-  image_scanning_configuration { scan_on_push = true }
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
