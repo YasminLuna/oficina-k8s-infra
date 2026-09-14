0537d25df1974b2f850800e62dc1e8182fc8cf59
YasminLuna <65403619+YasminLuna@users.noreply.github.com>
2026-09-14 11:07:23 -0300

    fix: corrige sintaxe das variáveis Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,20 @@
-variable "aws_region" { type = string, default = "us-east-1" }
-variable "environment" { type = string, default = "hml" }
-variable "cluster_name" { type = string, default = "oficina" }
-variable "datadog_api_key" { type = string, sensitive = true, default = "" }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "hml"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "oficina"
+}
+
+variable "datadog_api_key" {
+  type      = string
+  sensitive = true
+  default   = ""
+}
